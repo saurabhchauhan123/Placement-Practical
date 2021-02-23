b5cc0d6970b5cc06a548c232dce1d2c2001d309d
saurabhchauhan123 <71873770+saurabhchauhan123@users.noreply.github.com>
2021-02-23 18:20:09 +0530

    Add files via upload

---
+++ programs/program11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+int n=5;
+int i,j;
+for(i=0;i<n;i++){
+        for(j=0;j<i;j++)
+         printf(" ");
+     for(j=0;j<2*n-2*i-1;j++){
+            printf("*");
+     }
+
+    printf("\n");
+}
+
+}
---
+++ programs/program14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+int n=5;
+int i,j;
+for(i=0;i<n;i++){
+     for(j=0;j<i;j++){
+        printf(" ");
+     }
+    int k=i+1;
+    for(j=0;j<n-i;j++){
+            printf("%d ",k++);
+
+    }
+    printf("\n");
+}
+
+}
